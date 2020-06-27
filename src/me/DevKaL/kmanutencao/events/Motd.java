7a30a74f2b20c2cf5f1f37b54654b4c63d51f5ad
Kalel Santana <kalel.zzum@gmail.com>
2020-06-27 12:17:05 -0300

    Removido ServerListPing

--- src/me/DevKaL/kmanutencao/events/Motd.java
+++ src/me/DevKaL/kmanutencao/events/Motd.java
@@ -5,18 +5,24 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 import me.DevKaL.kmanutencao.main.Main;
 
-public class Motd implements Listener{
-	
+public class Motd implements Listener {
+
 	private final Main instance = Main.getMain();
-	
+
 	@EventHandler
 	public void motd(ServerListPingEvent e) {
-		if(!instance.getConfig().getBoolean("Manutencao.Situacao")) {
+		if (!instance.getConfig().getBoolean("Manutencao.Situacao")) {
 			String motd1 = instance.getConfig().getString("Server-MOTD.Linha-1").replace("&", "§");
 			String motd2 = instance.getConfig().getString("Server-MOTD.Linha-2").replace("&", "§");
 			int maxplayers = instance.getConfig().getInt("Server-MOTD.Max-Players");
 			e.setMotd(motd1 + '\n' + motd2);
 			e.setMaxPlayers(maxplayers);
+		} else {
+			String motd1 = instance.getConfig().getString("Manutencao-MOTD.Linha-1").replace("&", "§");
+			String motd2 = instance.getConfig().getString("Manutencao-MOTD.Linha-2").replace("&", "§");
+			int maxplayers = instance.getConfig().getInt("Manutencao-MOTD.Max-Players");
+			e.setMotd(motd1 + '\n' + motd2);
+			e.setMaxPlayers(maxplayers);
+		}
 	}
-  }
 }
